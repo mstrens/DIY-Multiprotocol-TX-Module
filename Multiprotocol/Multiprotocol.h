494c053361215b401456531fc99a42cb4e46c73c
midelic <midelic@gmail.com>
2022-08-18 13:27:32 +0100

    update definitions

--- Multiprotocol/Multiprotocol.h
+++ Multiprotocol/Multiprotocol.h
@@ -32,65 +32,65 @@ enum PROTOCOLS
 	PROTO_FLYSKY 	= 1,	// =>A7105
 	PROTO_HUBSAN	= 2,	// =>A7105
 	PROTO_FRSKYD	= 3,	// =>CC2500
-	PROTO_HISKY		= 4,	// =>NRF24L01
-	PROTO_V2X2		= 5,	// =>NRF24L01
-	PROTO_DSM		= 6,	// =>CYRF6936
-	PROTO_DEVO		= 7,	// =>CYRF6936
-	PROTO_YD717		= 8,	// =>NRF24L01
-	PROTO_KN		= 9,	// =>NRF24L01
-	PROTO_SYMAX		= 10,	// =>NRF24L01
-	PROTO_SLT		= 11,	// =>NRF24L01
-	PROTO_CX10		= 12,	// =>NRF24L01
-	PROTO_CG023		= 13,	// =>NRF24L01
+	PROTO_HISKY	= 4,	// =>NRF24L01
+	PROTO_V2X2	= 5,	// =>NRF24L01
+	PROTO_DSM	= 6,	// =>CYRF6936
+	PROTO_DEVO	= 7,	// =>CYRF6936
+	PROTO_YD717	= 8,	// =>NRF24L01
+	PROTO_KN	= 9,	// =>NRF24L01
+	PROTO_SYMAX	= 10,	// =>NRF24L01
+	PROTO_SLT	= 11,	// =>NRF24L01
+	PROTO_CX10	= 12,	// =>NRF24L01
+	PROTO_CG023	= 13,	// =>NRF24L01
 	PROTO_BAYANG	= 14,	// =>NRF24L01
 	PROTO_FRSKYX	= 15,	// =>CC2500
-	PROTO_ESKY		= 16,	// =>NRF24L01
+	PROTO_ESKY	= 16,	// =>NRF24L01
 	PROTO_MT99XX	= 17,	// =>NRF24L01
-	PROTO_MJXQ		= 18,	// =>NRF24L01
+	PROTO_MJXQ	= 18,	// =>NRF24L01
 	PROTO_SHENQI	= 19,	// =>NRF24L01
-	PROTO_FY326		= 20,	// =>NRF24L01
+	PROTO_FY326	= 20,	// =>NRF24L01
 	PROTO_FUTABA	= 21,	// =>CC2500
-	PROTO_J6PRO		= 22,	// =>CYRF6936
-	PROTO_FQ777		= 23,	// =>NRF24L01
-	PROTO_ASSAN		= 24,	// =>NRF24L01
+	PROTO_J6PRO	= 22,	// =>CYRF6936
+	PROTO_FQ777	= 23,	// =>NRF24L01
+	PROTO_ASSAN	= 24,	// =>NRF24L01
 	PROTO_FRSKYV	= 25,	// =>CC2500
 	PROTO_HONTAI	= 26,	// =>NRF24L01
 	PROTO_OPENLRS	= 27,	// =>OpenLRS hardware
 	PROTO_AFHDS2A	= 28,	// =>A7105
-	PROTO_Q2X2		= 29,	// =>NRF24L01, extension of CX-10 protocol
+	PROTO_Q2X2	= 29,	// =>NRF24L01, extension of CX-10 protocol
 	PROTO_WK2x01	= 30,	// =>CYRF6936
-	PROTO_Q303		= 31,	// =>NRF24L01
-	PROTO_GW008		= 32,	// =>NRF24L01
-	PROTO_DM002		= 33,	// =>NRF24L01
+	PROTO_Q303	= 31,	// =>NRF24L01
+	PROTO_GW008	= 32,	// =>NRF24L01
+	PROTO_DM002	= 33,	// =>NRF24L01
 	PROTO_CABELL	= 34,	// =>NRF24L01
 	PROTO_ESKY150	= 35,	// =>NRF24L01
-	PROTO_H8_3D		= 36,	// =>NRF24L01
+	PROTO_H8_3D	= 36,	// =>NRF24L01
 	PROTO_CORONA	= 37,	// =>CC2500
 	PROTO_CFLIE     = 38,   // =>NRF24L01
 	PROTO_HITEC     = 39,   // =>CC2500
-	PROTO_WFLY		= 40,	// =>CYRF6936
-	PROTO_BUGS		= 41,	// =>A7105
+	PROTO_WFLY	= 40,	// =>CYRF6936
+	PROTO_BUGS	= 41,	// =>A7105
 	PROTO_BUGSMINI	= 42,	// =>NRF24L01
 	PROTO_TRAXXAS	= 43,	// =>CYRF6936
 	PROTO_NCC1701	= 44,	// =>NRF24L01
-	PROTO_E01X		= 45,	// =>CYRF6936
-	PROTO_V911S		= 46,	// =>NRF24L01
-	PROTO_GD00X		= 47,	// =>NRF24L01
-	PROTO_V761		= 48,	// =>NRF24L01
-	PROTO_KF606		= 49,	// =>NRF24L01
+	PROTO_E01X	= 45,	// =>CYRF6936
+	PROTO_V911S	= 46,	// =>NRF24L01
+	PROTO_GD00X	= 47,	// =>NRF24L01
+	PROTO_V761	= 48,	// =>NRF24L01
+	PROTO_KF606	= 49,	// =>NRF24L01
 	PROTO_REDPINE	= 50,	// =>CC2500
 	PROTO_POTENSIC	= 51,	// =>NRF24L01
-	PROTO_ZSX		= 52,	// =>NRF24L01
+	PROTO_ZSX	= 52,	// =>NRF24L01
 	PROTO_HEIGHT	= 53,	// =>A7105
 	PROTO_SCANNER	= 54,	// =>CC2500
 	PROTO_FRSKY_RX	= 55,	// =>CC2500
 	PROTO_AFHDS2A_RX= 56,	// =>A7105
-	PROTO_HOTT		= 57,	// =>CC2500
-	PROTO_FX816		= 58,	// =>NRF24L01
+	PROTO_HOTT	= 57,	// =>CC2500
+	PROTO_FX816	= 58,	// =>NRF24L01
 	PROTO_BAYANG_RX	= 59,	// =>NRF24L01
 	PROTO_PELIKAN	= 60,	// =>A7105
-	PROTO_TIGER		= 61,	// =>NRF24L01
-	PROTO_XK		= 62,	// =>NRF24L01
+	PROTO_TIGER	= 61,	// =>NRF24L01
+	PROTO_XK	= 62,	// =>NRF24L01
 	PROTO_XN297DUMP	= 63,	// =>NRF24L01
 	PROTO_FRSKYX2	= 64,	// =>CC2500
 	PROTO_FRSKY_R9	= 65,	// =>SX1276
@@ -100,29 +100,29 @@ enum PROTOCOLS
 	PROTO_ESKY150V2	= 69,	// =>CC2500+NRF24L01
 	PROTO_DSM_RX	= 70,	// =>CYRF6936
 	PROTO_JJRC345	= 71,	// =>NRF24L01
-	PROTO_Q90C		= 72,	// =>NRF24L01 or CC2500
+	PROTO_Q90C	= 72,	// =>NRF24L01 or CC2500
 	PROTO_KYOSHO	= 73,	// =>A7105
-	PROTO_RLINK		= 74,	// =>CC2500
+	PROTO_RLINK	= 74,	// =>CC2500
 	PROTO_REALACC	= 76,	// =>NRF24L01
-	PROTO_OMP		= 77,	// =>CC2500 & NRF24L01
-	PROTO_MLINK		= 78,	// =>CYRF6936
-	PROTO_WFLY2		= 79,	// =>A7105
+	PROTO_OMP	= 77,	// =>CC2500 & NRF24L01
+	PROTO_MLINK	= 78,	// =>CYRF6936
+	PROTO_WFLY2	= 79,	// =>A7105
 	PROTO_E016HV2	= 80,	// =>CC2500 & NRF24L01
 	PROTO_E010R5	= 81,	// =>CYRF6936
-	PROTO_LOLI		= 82,	// =>NRF24L01
-	PROTO_E129		= 83,	// =>CYRF6936
+	PROTO_LOLI	= 82,	// =>NRF24L01
+	PROTO_E129	= 83,	// =>CYRF6936
 	PROTO_JOYSWAY	= 84,	// =>A7105
-	PROTO_E016H		= 85,	// =>NRF24L01
+	PROTO_E016H	= 85,	// =>NRF24L01
 	PROTO_CONFIG	= 86,	// Module config
 	PROTO_IKEAANSLUTA = 87, // =>CC2500
 	PROTO_WILLIFM	= 88,	// 27/35ab/40/41/72 MHz module external project
-	PROTO_LOSI		= 89,	// =>CYRF6936
+	PROTO_LOSI	= 89,	// =>CYRF6936
 	PROTO_MOULDKG	= 90,	// =>NRF24L01
 	PROTO_XERALL	= 91,	// =>NRF24L01
 	PROTO_MT99XX2	= 92,	// =>NRF24L01, extension of MT99XX protocol
 	PROTO_NANORF	= 126,	// =>NRF24L01
-	PROTO_TEST		= 127,	// =>CC2500
-	PROTO_MILO       = 128 //SX1280
+	PROTO_TEST	= 127,	// =>CC2500
+	PROTO_MILO      = 128   //SX1280
 };
 
 enum Flysky
@@ -463,10 +463,10 @@ enum KF606
 
 enum MILO
 {
-	MCH_16		= 0,
-	MCH_8		    = 1,
-    WIFI_TX       = 2,
-	WIFI_RX      = 3,
+	MCH_16	    = 0,
+	MCH_8	    = 1,
+        WIFI_TX     = 2,
+	WIFI_RX     = 3,
 };
 
 
@@ -649,12 +649,12 @@ enum MultiPacketTypes
 //********************
 //** Debug messages **
 //********************
-#if (defined (STM32_BOARD)||defined( ESP32_PLATFORM)) && (defined (DEBUG_SERIAL) || defined (ARDUINO_MULTI_DEBUG))
+#if (defined (STM32_BOARD)||defined( ESP_COMMON)) && (defined (DEBUG_SERIAL) || defined (ARDUINO_MULTI_DEBUG))
 	uint16_t debug_time=0;
 	char debug_buf[64];
 	#define debug(msg, ...)  { sprintf(debug_buf, msg, ##__VA_ARGS__); Serial.write(debug_buf);}
 	#define debugln(msg, ...)  { sprintf(debug_buf, msg "\r\n", ##__VA_ARGS__); Serial.write(debug_buf);}
-	#if defined( ESP32_PLATFORM)
+	#if defined( ESP_COMMON)
 		#define debug_time(msg)  { uint32_t debug_time_TCNT1 = timerRead(timer); debug_time=debug_time_TCNT1-debug_time; debug(msg "%u", debug_time>>1); debug_time=debug_time_TCNT1; }
 		#define debugln_time(msg)  { uint32_t debug_time_TCNT1 = timerRead(timer); debug_time=debug_time_TCNT1-debug_time; debug(msg "%u\r\n", debug_time>>1); debug_time=debug_time_TCNT1; }
 		#else
